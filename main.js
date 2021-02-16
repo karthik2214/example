05f86ba9ab73ba03954025708991199b98e418e5
karthik2214 <71806384+karthik2214@users.noreply.github.com>
2021-02-16 14:09:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+`use strict`;
+
+let name = "karthik";
+let age = 15;
+
+if (12<age && (name[0] === "k" || name[0] === "K")) {
+  console.log("if block");
+} else {
+  console.log("else block");
+}
